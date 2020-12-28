36ffa06d4f8c99363a12a0cb9386bdcc0d77b6e6
rich3664 <rich3664@gmail.com>
2020-12-28 17:01:38 +0900

    블레이드

--- www/resources/views/welcome.blade.php
+++ www/resources/views/welcome.blade.php
@@ -1,1 +1,13 @@
-<h1><?= isset($greeting) ? "{$greeting}" : 'Hello';?><?= $name;?></h1>
+@extends('layouts.master')
+
+@section('content')
+    <p>저는 자식 뷰의 'content' 섹션입니다.ddd</p>
+    @include('partials.footer')
+@endsection
+
+@section('script')
+    @parent
+    <script>
+        alert('저는 자식뷰의 스크립트 섹션입니다.');
+    </script>
+@endsection
